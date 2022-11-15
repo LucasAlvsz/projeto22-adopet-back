245dccc3ace81c0b9608023fa6ec507a1e80e8ef
Lucas A <contato.lucasalv@gmail.com>
2022-11-15 10:32:09 -0300

    refactor: remove unused imports

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import { serverHttp } from "@/app"
-import "@/controllers/chatController"
 
 serverHttp.listen(process.env.PORT || 5000, () =>
 	console.log(`Server running on port ${process.env.PORT || 5000} 🐱‍👤`)
