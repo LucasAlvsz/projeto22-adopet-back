70978dde8ed2d522927480377021a805df93e7cd
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-12-02 18:54:06 -0300

    fix: removing socket

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,12 @@
 import express from "express"
 import cors from "cors"
-import http from "http"
-import { Server } from "socket.io"
 import router from "@/routes"
-import axios from "axios"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const serverHttp = http.createServer(app)
-const io = new Server(serverHttp, {
-	cors: {
-		origin: "http://localhost:5173",
-		methods: ["GET", "POST"],
-		allowedHeaders: ["Authorization"],
-		credentials: true,
-	},
-})
-
-app.post("/", (req, res) => {
-	axios
-		.post(
-			"https://api.imgbb.com/1/upload",
-			{
-				image: req.body,
-			},
-			{
-				headers: {
-					key: "1f23f7ea194499ef2613067439ec958c",
-				},
-			}
-		)
-		.then(({ data }) => {
-			res.send(data)
-		})
-		.catch(err => {
-			console.error(err)
-			res.sendStatus(500)
-		})
-})
-
 app.get("/", (req, res) => res.send("Hello World!"))
 
-export { serverHttp, io }
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
-import { serverHttp } from "@/app"
-import "@/controllers/chatController"
+import app from "@/app"
 
-serverHttp.listen(process.env.PORT || 5000, () =>
+app.listen(process.env.PORT || 5000, () =>
 	console.log(`Server running on port ${process.env.PORT || 5000} 🐱‍👤`)
 )
